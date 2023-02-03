c03519554a3b29edd27b0b7c5a9234f94429666e
Ola <ola.ulvas@gmail.com>
2023-02-03 23:55:44 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+
+print("Welcome to the Band-Name-Generator!\n")
+city = input("Which city did you grow up in?\n ")
+pet = input("What's the name of your favorite pet?\n ")
+print(f"Your band name could be {city} {pet}")
